181cdef28f7179b2c0325fcf4a8ac440bac4601c
Andy Stewart <lazycat.manatee@gmail.com>
2012-04-21 14:49:43 +0800

    Add repos.sh

---
+++ repos.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+case "$1" in
+    "record" )
+        git commit -a
+        ;;
+    "pull" )
+        git pull origin master
+        ;;
+    "push" )
+        git push git@github.com:manateelazycat/deepin-gnome-shell-extensions.git
+        ;;
+    "revert" )
+        git checkout -- .
+        ;;
+    * ) 
+        echo "Help"
+        echo "./repos.sh record         => record patch"
+        echo "./repos.sh pull           => pull patch"
+        echo "./repos.sh push           => push patch"
+        echo "./repos.sh revert         => revert patch"
+        ;;
+    esac
