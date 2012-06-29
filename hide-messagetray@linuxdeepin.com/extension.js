0688de4ab2cf8b0a62827ba34892db81ad85d0ec
Andy Stewart <lazycat.manatee@gmail.com>
2012-06-29 12:34:30 +0800

    Add panel-settings extension and fix hide-messagetray extension.

--- hide-messagetray@linuxdeepin.com/extension.js
+++ hide-messagetray@linuxdeepin.com/extension.js
@@ -2,15 +2,24 @@ const Lang = imports.lang;
 const Main = imports.ui.main;
 
 let messageTrayActor;
+let viewSelectorActor;
+let viewSelectorActorWidth;
+let viewSelectorActorHeight;
 
 function init() {
 	messageTrayActor = Main.messageTray.actor;
 }
 
 function enable() {
+	viewSelectorActor = Main.overview._group.get_children()[0];
+	[viewSelectorActorWidth, viewSelectorActorHeight] = viewSelectorActor.get_size();
+	viewSelectorActor.set_size(viewSelectorActorWidth, viewSelectorActorHeight + 10);
+	
 	messageTrayActor.hide();
 }
 
 function disable() {
+	viewSelectorActor.set_size(viewSelectorActorWidth, viewSelectorActorHeight);
+	
 	messageTrayActor.show();
 }
